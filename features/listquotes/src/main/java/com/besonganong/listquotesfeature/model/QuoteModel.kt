d0d0d0dacea6de86aaf77ca32c73fd1d738ff479
besongernest <besonganong_ernest@yahoo.com>
2024-01-06 14:25:26 -0330

    Change Quote to QuoteModel.

---
+++ features/listquotes/src/main/java/com/besonganong/listquotesfeature/model/QuoteModel.kt
@@ -0,0 +1,8 @@
+package com.besonganong.listquotesfeature.model
+
+
+/**
+ * Model class that represents a Quote.
+ */
+
+data class QuoteModel(val text: String, val characterCount: Int, val author: String)
