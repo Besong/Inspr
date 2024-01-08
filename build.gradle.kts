45096cafbb14fff0dfaf070b62ffb404bd3c4486
besongernest <besonganong_ernest@yahoo.com>
2024-01-08 07:50:38 -0330

    Update the project build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.android.dynamic-feature") version "8.1.2" apply false
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0" apply false
+    kotlin("jvm") version "1.9.20" apply false
+    kotlin("plugin.serialization") version "1.9.20" apply false
 
 }
