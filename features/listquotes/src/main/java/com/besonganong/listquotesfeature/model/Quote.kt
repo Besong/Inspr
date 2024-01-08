415318ac06d0ca820cb1a049ceb23eaa191c3704
besongernest <besonganong_ernest@yahoo.com>
2024-01-08 08:02:45 -0330

    Remove Quote for QuoteModel.

--- features/listquotes/src/main/java/com/besonganong/listquotesfeature/model/Quote.kt
+++
@@ -1,6 +0,0 @@
-package com.besonganong.listquotesfeature.model
-
-/**
- * Class that models a Quote from the zenquotes.io API
- */
-data class Quote(var text: String, var characterCount: Int, var author: String)
