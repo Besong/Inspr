1358dbcfebc23cbe55df451de7b6e2d79a8e1fc2
besongernest <besonganong_ernest@yahoo.com>
2024-01-08 08:11:55 -0330

    Remove QuoteScreenUIState for QuoteScreenState

--- features/listquotes/src/main/java/com/besonganong/listquotesfeature/model/QuotesScreenUIState.kt
+++
@@ -1,6 +0,0 @@
-package com.besonganong.listquotesfeature.model
-
-/**
- * Class representing the UI state of QuotesScreen.
- */
-data class QuotesScreenUIState(val quotesScreenState: QuotesScreenState)
