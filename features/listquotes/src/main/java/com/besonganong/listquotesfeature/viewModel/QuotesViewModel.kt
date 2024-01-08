8891c1aa3c42a7c89d4c0f70d3ba2ed239db9899
besongernest <besonganong_ernest@yahoo.com>
2024-01-08 07:27:08 -0330

    Add ViewModel for the Quotes screen

---
+++ features/listquotes/src/main/java/com/besonganong/listquotesfeature/viewModel/QuotesViewModel.kt
@@ -0,0 +1,49 @@
+package com.besonganong.listquotesfeature.viewModel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.besonganong.listquotesfeature.data.repository.QuotesRepository
+import com.besonganong.listquotesfeature.model.QuotesScreenState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel used for providing states to the QuotesScreen
+ */
+class QuotesViewModel (
+    private val quotesRepository: QuotesRepository
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow<QuotesScreenState>(QuotesScreenState.Loading)
+
+    val uiState: StateFlow<QuotesScreenState> = _uiState
+
+
+    init {
+        updateState()
+    }
+
+    fun onErrorButtonClick() {
+        updateState()
+    }
+
+    private fun updateState() {
+        viewModelScope.launch {
+
+            quotesRepository.getQuotes()
+                .catch {cause ->
+                    cause.message?.let {
+                        Log.e(this@QuotesViewModel.toString(), it)
+                    }
+
+                    _uiState.value = QuotesScreenState.Error
+                }
+                .collect { latestQuotes ->
+                    _uiState.value = QuotesScreenState.Success(latestQuotes)
+                }
+        }
+    }
+}
