f781122ffae08aa04ac728257ccd2550cecebddf
besongernest <besonganong_ernest@yahoo.com>
2024-08-21 11:02:22 -0230

    Update the Error class with string resources for an error message

--- features/listquotes/src/main/java/com/besonganong/listquotesfeature/model/QuoteScreenState.kt
+++ features/listquotes/src/main/java/com/besonganong/listquotesfeature/model/QuoteScreenState.kt
@@ -1,11 +1,13 @@
 package com.besonganong.listquotesfeature.model
 
+import androidx.annotation.StringRes
+
 /**
  *  Class representing the different UI states of QuotesScreen:
  *  Loading, Error and Success
  */
 sealed class QuotesScreenState {
     data class Success(val quoteModels: List<QuoteModel>): QuotesScreenState()
-    data object Error: QuotesScreenState()
+    data class Error(@StringRes val resId: Int): QuotesScreenState()
     data object Loading: QuotesScreenState()
 }
