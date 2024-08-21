0c991625f1bfb724948c91fce6751d40f4a70fe6
besongernest <besonganong_ernest@yahoo.com>
2024-08-21 10:56:06 -0230

    Rename ErrorIndicator to ErrorMessage and update text properties to access string resources

--- features/listquotes/src/main/java/com/besonganong/listquotesfeature/view/ui/components/ErrorMessage.kt
+++ features/listquotes/src/main/java/com/besonganong/listquotesfeature/view/ui/components/ErrorMessage.kt
@@ -1,5 +1,6 @@
 package com.besonganong.listquotesfeature.view.ui.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,14 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.besonganong.listquotesfeature.R
 
 /**
  * A simple composable that displays an error message.
  */
 @Composable
-fun ErrorIndicator(
+fun ErrorMessage(
+    @StringRes errorMsg: Int,
     onErrorButtonClick: () -> Unit
 ) {
 
@@ -26,11 +30,11 @@ fun ErrorIndicator(
         modifier = Modifier.fillMaxSize()
     ) {
 
-    Text(text = " Sorry!. Can't Load Quotes.", modifier = Modifier.padding(bottom = 22.dp))
+    Text(text = stringResource(id = errorMsg), modifier = Modifier.padding(bottom = 22.dp))
 
     Button(onClick = onErrorButtonClick) {
 
-        Text(text = "Try Again!")
+        Text(text = stringResource(id = R.string.try_again))
         }
     }
 }
@@ -39,5 +43,5 @@ fun ErrorIndicator(
 @Composable
 fun ErrorMessagePreview() {
     
-    ErrorIndicator {}
+    ErrorMessage(errorMsg = R.string.network_is_disconnected) {}
 }
